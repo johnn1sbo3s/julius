84c75aaaa0baceb4bc7eff4a5a6be069da217f23
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-08-30 00:44:46 -0300

    Cria composable de requisições

---
+++ frontend/src/js/shared/composables/useApi.js
@@ -0,0 +1,28 @@
+import { ref } from 'vue'
+
+export function useApi(service) {
+	const data = ref(null);
+	const errorMsg = ref('');
+	const loading = ref(false);
+
+	async function request(...args) {
+		loading.value = true;
+
+		try {
+			data.value = await service(...args);
+			return data.value?.data
+		} catch (error) {
+			errorMsg.value = error.response?.data || error.message
+			throw error
+		} finally {
+			loading.value = false;
+		}
+	}
+
+	return {
+		data,
+		loading,
+		error: errorMsg,
+		request,
+	}
+}

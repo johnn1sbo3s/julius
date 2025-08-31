edac2a491d0c21aee5f64f023370eb62aaf7c3e4
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-08-31 17:48:01 -0300

    Corrige estilo de código

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
 	baseURL: 'http://localhost:8000/api/v1',
 	timeout: 10000,
-	headers: { 'Content-Type': 'application/json'},
+	headers: { 'Content-Type': 'application/json' },
 });
 
 api.interceptors.request.use((config) => {

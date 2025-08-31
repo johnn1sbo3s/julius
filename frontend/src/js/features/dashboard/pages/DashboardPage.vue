6d1148d48b43fcdc72be4435fb3a2671eee3b4be
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-08-31 17:05:49 -0300

    Inicia a criação dos elementos da dashboard

--- frontend/src/js/features/dashboard/pages/DashboardPage.vue
+++ frontend/src/js/features/dashboard/pages/DashboardPage.vue
@@ -1,14 +1,31 @@
 <template>
 	<main class="flex-grow flex flex-col gap-2 items-center justify-between">
-		<img
-			src="/img/logo.svg"
-			alt="Logo"
-			class="w-40 mt-6"
-		>
+		<section class="flex flex-col w-full">
+			<h2 class="font-semibold">
+				E aí, Joneta!
+			</h2>
 
-		<h1 class="font-semibold">
-			Dashboard
-		</h1>
+			<p>Vamos controlar os gastos juntos.</p>
+		</section>
+
+		<section class="flex flex-col gap-4 w-full">
+			<h3 class="font-semibold">
+				Gastos por categoria
+			</h3>
+
+			<div class="grid grid-cols-2 w-full gap-2">
+				<UCard
+					v-for="category in categories"
+					:key="category.id"
+					class="w-full"
+				>
+					<div>
+						Chama
+					</div>
+				</UCard>
+			</div>
+		</section>
+
 
 		<UModal
 			title="Novo gasto"
@@ -30,15 +47,16 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import TransactionForm from '@/js/features/dashboard/components/TransactionForm.vue'
 import { useApi } from '@/js/shared/composables/useApi'
 import { getCategories } from '@/js/features/dashboard/services/categories'
-import { onMounted } from 'vue';
 
-const { request: getCategoriesRequest, data, error, loading } = useApi(getCategories);
+const { request: getCategoriesRequest, data: categories, error, loading } = useApi(getCategories);
 
 onMounted(async () => {
 	await getCategoriesRequest()
+	console.log(categories.value)
 })
 
 </script>

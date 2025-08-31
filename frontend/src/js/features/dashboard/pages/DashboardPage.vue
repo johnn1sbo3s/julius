a42255f40ea7e25e9a964e9eea47461f424ae67f
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-08-31 13:57:09 -0300

    Adiciona a busca de categorias na api

--- frontend/src/js/features/dashboard/pages/DashboardPage.vue
+++ frontend/src/js/features/dashboard/pages/DashboardPage.vue
@@ -6,14 +6,16 @@
 			class="w-40 mt-6"
 		>
 
-		<h1>Dashboard</h1>
+		<h1 class="font-semibold">
+			Dashboard
+		</h1>
 
 		<UModal
 			title="Novo gasto"
 			:dismissible="false"
 		>
 			<UButton
-				class="w-full justify-center mb-8"
+				class="w-full justify-center mb-8 cursor-pointer"
 				size="xl"
 				icon="i-lucide-wallet-minimal"
 			>
@@ -27,7 +29,16 @@
 	</main>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 import TransactionForm from '@/js/features/dashboard/components/TransactionForm.vue'
+import { useApi } from '@/js/shared/composables/useApi'
+import { getCategories } from '@/js/features/dashboard/services/categories'
+import { onMounted } from 'vue';
+
+const { request: getCategoriesRequest, data, error, loading } = useApi(getCategories);
+
+onMounted(async () => {
+	await getCategoriesRequest()
+})
 
 </script>
